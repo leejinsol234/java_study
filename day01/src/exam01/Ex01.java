1b7318fda25ba637b871d997e98df1bac9567d80
leejinsol234 <desirable7@naver.com>
2023-10-10 23:01:43 +0900

    Initial commit

---
+++ day01/src/exam01/Ex01.java
@@ -0,0 +1,13 @@
+package exam01;
+
+public class Ex01 {
+
+    public static void main(String[] args) {
+        int num;
+        num = 10;
+
+        int num2=10;
+
+        double num3 = 10.123;
+    }
+}
